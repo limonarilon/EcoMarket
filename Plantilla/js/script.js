cadaddf83e62c59f0943e3b4c0688c0acffefac4
Ariel Silva Ortega <ar.silvao@duocuc.cl>
2025-09-02 20:52:28 -0400

    corrección de errores formulario
    el formulario fallaba porque el preloader se quedaba cargando para siempre, le puse un timeout de 3 segundos para que cargue el formulario y ahora si genera el modal de registro :D

--- Plantilla/js/script.js
+++ Plantilla/js/script.js
@@ -1,161 +1,103 @@
 (function($) {
-
   "use strict";
 
   var initPreloader = function() {
-    $(document).ready(function($) {
-    var Body = $('body');
-        Body.addClass('preloader-site');
-    });
-    $(window).load(function() {
+    $(document).ready(function() {
+      $('body').addClass('preloader-site');
+
+      // Ocultar preloader tras 3 segundos si no se carga todo
+      setTimeout(function() {
         $('.preloader-wrapper').fadeOut();
         $('body').removeClass('preloader-site');
+      }, 3000);
+    });
+
+    $(window).on('load', function() {
+      $('.preloader-wrapper').fadeOut();
+      $('body').removeClass('preloader-site');
     });
   }
 
-  // init Chocolat light box
-	var initChocolat = function() {
-		Chocolat(document.querySelectorAll('.image-link'), {
-		  imageSize: 'contain',
-		  loop: true,
-		})
-	}
+  var initChocolat = function() {
+    if (typeof Chocolat !== 'undefined') {
+      Chocolat(document.querySelectorAll('.image-link'), {
+        imageSize: 'contain',
+        loop: true,
+      });
+    }
+  }
 
   var initSwiper = function() {
+    if (typeof Swiper !== 'undefined') {
+      new Swiper(".main-swiper", {
+        speed: 500,
+        pagination: { el: ".swiper-pagination", clickable: true },
+      });
 
-    var swiper = new Swiper(".main-swiper", {
-      speed: 500,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+      new Swiper(".category-carousel", {
+        slidesPerView: 6,
+        spaceBetween: 30,
+        speed: 500,
+        navigation: { nextEl: ".category-carousel-next", prevEl: ".category-carousel-prev" },
+        breakpoints: { 0:{slidesPerView:2},768:{slidesPerView:3},991:{slidesPerView:4},1500:{slidesPerView:6} }
+      });
 
-    var category_swiper = new Swiper(".category-carousel", {
-      slidesPerView: 6,
-      spaceBetween: 30,
-      speed: 500,
-      navigation: {
-        nextEl: ".category-carousel-next",
-        prevEl: ".category-carousel-prev",
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1500: {
-          slidesPerView: 6,
-        },
-      }
-    });
+      new Swiper(".brand-carousel", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        speed: 500,
+        navigation: { nextEl: ".brand-carousel-next", prevEl: ".brand-carousel-prev" },
+        breakpoints: { 0:{slidesPerView:2},768:{slidesPerView:2},991:{slidesPerView:3},1500:{slidesPerView:4} }
+      });
 
-    var brand_swiper = new Swiper(".brand-carousel", {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      speed: 500,
-      navigation: {
-        nextEl: ".brand-carousel-next",
-        prevEl: ".brand-carousel-prev",
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        991: {
-          slidesPerView: 3,
-        },
-        1500: {
-          slidesPerView: 4,
-        },
-      }
-    });
-
-    var products_swiper = new Swiper(".products-carousel", {
-      slidesPerView: 5,
-      spaceBetween: 30,
-      speed: 500,
-      navigation: {
-        nextEl: ".products-carousel-next",
-        prevEl: ".products-carousel-prev",
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1500: {
-          slidesPerView: 6,
-        },
-      }
-    });
+      new Swiper(".products-carousel", {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        speed: 500,
+        navigation: { nextEl: ".products-carousel-next", prevEl: ".products-carousel-prev" },
+        breakpoints: { 0:{slidesPerView:1},768:{slidesPerView:3},991:{slidesPerView:4},1500:{slidesPerView:6} }
+      });
+    }
   }
 
   var initProductQty = function(){
-
     $('.product-qty').each(function(){
-
-      var $el_product = $(this);
-      var quantity = 0;
-
-      $el_product.find('.quantity-right-plus').click(function(e){
-          e.preventDefault();
-          var quantity = parseInt($el_product.find('#quantity').val());
-          $el_product.find('#quantity').val(quantity + 1);
+      var $el = $(this);
+      $el.find('.quantity-right-plus').click(function(e){
+        e.preventDefault();
+        var qty = parseInt($el.find('#quantity').val());
+        $el.find('#quantity').val(qty + 1);
       });
-
-      $el_product.find('.quantity-left-minus').click(function(e){
-          e.preventDefault();
-          var quantity = parseInt($el_product.find('#quantity').val());
-          if(quantity>0){
-            $el_product.find('#quantity').val(quantity - 1);
-          }
+      $el.find('.quantity-left-minus').click(function(e){
+        e.preventDefault();
+        var qty = parseInt($el.find('#quantity').val());
+        if(qty>0){ $el.find('#quantity').val(qty - 1); }
       });
-
     });
-
   }
 
-  // init jarallax parallax
   var initJarallax = function() {
-    jarallax(document.querySelectorAll(".jarallax"));
-
-    jarallax(document.querySelectorAll(".jarallax-keep-img"), {
-      keepImg: true,
-    });
+    if (typeof jarallax !== 'undefined') {
+      jarallax(document.querySelectorAll(".jarallax"));
+      jarallax(document.querySelectorAll(".jarallax-keep-img"), { keepImg: true });
+    }
   }
 
-  // document ready
   $(document).ready(function() {
-    
     initPreloader();
     initSwiper();
     initProductQty();
     initJarallax();
     initChocolat();
 
-    // Modal al enviar solo el formulario de registro
-    var $form = $('#registroForm');
-    if ($form.length && $('#modalEnvio').length) {
-      $form.on('submit', function(e) {
-        e.preventDefault();
-        var modal = new bootstrap.Modal(document.getElementById('modalEnvio'));
-        modal.show();
-      });
-    }
-  }); // End of a document
+    // Modal al enviar formulario de registro
+    $('#registroForm').on('submit', function(e) {
+      e.preventDefault();
+      var modal = new bootstrap.Modal(document.getElementById('modalEnvio'));
+      modal.show();
+      // Opcional: resetear formulario
+      this.reset();
+    });
+  });
 
 })(jQuery);
